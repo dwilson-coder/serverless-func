f9046d237a51fc8796e6cf6981f7f02fa3b9f6fd
D. Wilson <34779401+dwilson-coder@users.noreply.github.com>
2023-06-19 19:56:17 -0400

    Add files via upload

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items = require("../assets/data");
 
 exports.handler = async (events, context) => {
   return {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   };
